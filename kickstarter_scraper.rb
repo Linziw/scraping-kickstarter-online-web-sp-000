b12554cbba866bd83945572daafb40109e63a2e8
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-24 06:19:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,3 +14,18 @@ end
 # location: project.css("span.location-name").text
 # percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
+# file: kickstarter_scraper.rb
+ 
+...
+ 
+def create_project_hash
+  projects = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+ 
+  # return the projects hash
+  projects
+end
